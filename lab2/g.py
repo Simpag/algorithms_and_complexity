6921c933cdbddb007b719118c31f7e72b8831003
Simon ubuntu <simon.granstrom@gmail.com>
2023-04-21 18:07:22 +0200

    lab2 done

--- lab2/g.py
+++ lab2/g.py
@@ -1,3 +1,4 @@
+import math
 from matplotlib import pyplot as plt
 import networkx as nx
 
@@ -30,16 +31,6 @@ def graph(n, s, k, constraints_type_1, constraints_type_2, p1, p2):
     for role in range(1, n+1):
         G.add_node(f"R{role}")
 
-    # Add nodes for each actor
-    # for actor in range(1, k+1):
-    #     G.add_node(f"A{actor}")
-
-    # Add edges for constraints of type 1
-    # I.e add edges between every role that an actor can play
-    # for role, actors in constraints_type_1.items():
-    #     for actor in actors:
-    #         G.add_edge(f"R{role}", f"A{actor}")
-
 
     # Add edges for constraints of type 2
     # I.e add an edge between every role that appear in the same scene
@@ -48,81 +39,42 @@ def graph(n, s, k, constraints_type_1, constraints_type_2, p1, p2):
             for j in range(i+1, len(roles)):
                 G.add_edge(f"R{roles[i]}", f"R{roles[j]}")
 
-    # Add edges for the divas not playing together
-    # I.e add an edge between all roles that the divas can play
-    # for role in range(1, n+1):
-    #     #if role not in constraints_type_1[p1] or role not in constraints_type_1[p2]:
-    #     #    G.add_edge(f"R{role}", "Divas")
-        
-    #     # add an edge between all roles that p1 and p2 can play
-    #     if p1 in constraints_type_1[role] and p2 in constraints_type_1[role]:
-    #         G.add_edge(f"R{role}", "Divas")
-
-    diva_roles = []
-    for role in constraints_type_1:
-        if p1 in constraints_type_1[role] or p2 in constraints_type_1[role]:
-            diva_roles.append(role)
-
-    # print("Diva roles")
-    # print(diva_roles)
-    # for roles_in_scene in constraints_type_2:
-    #     d = [value for value in diva_roles if value in roles_in_scene]
-
-    #     if len(d) > 1:
-    #         for role in d:
-    #             G.add_edge(f"R{role}", "Divas")
-        
 
     # Set position for nodes
-    # pos = {}
-    # for role in range(1, n+1):
-    #     pos[f"R{role}"] = (1+role/20*(-1)**role, role)
-    # for actor in range(1, k+1):
-    #     pos[f"A{actor}"] = (2, actor)
-    # pos["Divas"] = (3, 1.5)
+    pos = {}
+    for role in range(n):
+        pos[f"R{role+1}"] = (-math.cos(role/(n)*2*math.pi), math.sin(role/(n)*2*math.pi))
+    
 
-    return G#, pos
+    return G, pos
 
 
-#input_string = "6\n5\n4\n3 1 3 4\n2 2 3\n2 1 3\n1 2\n4 1 2 3 4\n2 1 4\n3 1 2 6\n3 2 3 5\n3 2 4 6\n3 2 3 6\n2 1 6"
-# input_string = """5
-# 5
-# 3
-# 3 1 2 3
-# 2 2 3
-# 2 1 3
-# 1 2
-# 3 1 2 3
-# 2 1 2
-# 2 1 2
-# 3 1 3 4
-# 2 3 5
-# 3 2 3 5"""
+
 def run(input_string):
     n, s, k, constraints_type_1, constraints_type_2, p1, p2 = parse_input(input_string)
-    G = graph(n, s, k, constraints_type_1, constraints_type_2, p1, p2)
-    print(constraints_type_1)
-    print("\n")
-    print(constraints_type_2)
+    G, pos = graph(n, s, k, constraints_type_1, constraints_type_2, p1, p2)
+    #print(constraints_type_1)
+    #print("\n")
+    #print(constraints_type_2)
 
     # Find the coloring if possible
-    # colors = nx.greedy_color(G)
-    # uniqueColors = set(colors.values())
+    colors = nx.greedy_color(G)
+    uniqueColors = set(colors.values())
 
-    # print(f'Can be colored with {len(uniqueColors)} colors')
-    # print(f"k: {k}")
+    print(f'Can be colored with {len(uniqueColors)} colors')
+    print(f"k: {k}")
 
     color_list = ["gold", "lightblue", "limegreen", "orange", 'red', 'blue']
     nodes = list(G.nodes())
     node_colors = [0, ] * len(nodes)
-    for node in constraints_type_1:
-        i = nodes.index(f'R{node}')
-        node_colors[i] = color_list[constraints_type_1[node][0]]
+    for node in colors:
+        i = nodes.index(node)
+        node_colors[i] = color_list[colors[node]]
 
     print(set(node_colors))
     assert(len(nodes) == len(node_colors))
 
 
     # Draw graph
-    nx.draw(G, node_color=node_colors, with_labels=True, font_weight='bold')
+    nx.draw(G, pos, node_color=node_colors, with_labels=True, font_weight='bold')
     plt.show()
--- lab2/main.py
+++ lab2/main.py
@@ -3,20 +3,24 @@
 # E = 0 (no scenes)
 # m < 2
 
+from g import run as draw_graph
+
 def run():
-    negative_output = """5
-    5
-    3
-    3 1 2 3
-    2 2 3
-    2 1 3
+    negative_output = """2
+    1
+    2
+    1 1
     1 2
-    3 1 2 3
-    2 1 2
-    2 1 2
-    3 1 3 4
-    2 3 5
-    3 2 3 5"""
+    2 1 2"""
+
+    positive_output = """3
+    2
+    3
+    1 1
+    1 2
+    1 3
+    2 1 3
+    2 2 3"""
 
     V = int(input())
     E = int(input())
@@ -26,28 +30,50 @@ def run():
     scenes = []
     edges = []
     isolated_vertex = [True,] * V # Keep track of all isolated verticies
+    # O(E)
     for _ in range(E):
         u, v = map(int, input().split())
         isolated_vertex[u-1] = False
         isolated_vertex[v-1] = False
         edges.append((u,v) if u < v else (v,u)) # add the in increasing order
 
+    # O(V)
+    # Cant have monolouges but adding an isolated vertex to the graph does
+    # not affect if it can be colored or not since the isolated vertex can
+    # take any of the colors that the vertex in the graph is close to
     for i, is_isolated in enumerate(isolated_vertex):
         if is_isolated:
             # add an edge to anywhere
             edges.append((3, i+1))
-
-    # Minimum requirements for a possible solution
-    if m < 3 or V < 3 or E < 2:
-        print(negative_output)
-        return
 
     # Add one role and one actor to the problem to account
     # for the divas
     n = V+1
     k = m+1
 
+    # Minimum requirements for a possible solution of the vertex coloring problem
+    if V == 0 and E == 0 and m == 0:
+        print(positive_output)
+        return
+    
+    if m < 2 and E > 0:
+        print(negative_output)
+        return
+    
+    if E == 0 and m > 0:
+        print(positive_output)
+        return
+
+    # Minimum requirements for a possible solution of the casting problem
+    # 3 Roles  (# verticies)
+    # 2 Scenes (# edges)
+    # 3 Actors (# colors)
+    if k < 3 or len(edges) < 2 or n < 3:
+        print(negative_output)
+        return
+
     # Each edge is a scene, add one to avoid using role 1
+    # O(E+c) where c is some constant (all the isolated verticies)
     for r_1, r_2 in edges:
         # now add to scenes
         scenes.append(set([r_1+1, r_2+1]))
@@ -58,16 +84,28 @@ def run():
 
     # Every actor can play every role except for role 1
     # Role 1 is reserved for Diva 1
+    # O(n)
     roles = [list(range(1,k+1)) for x in range(n)]
     roles[0] = [1]
+    # Since diva 1 is connected to play with role 3,
+    # role 3 can not be played by diva 2
+    roles[2].remove(2)
 
 
-    print(n)
-    print(len(scenes))
-    print(k)
+    output = str(n) + '\n'
+    output += str(len(scenes)) + '\n'
+    output += str(k)  + '\n'
     for item in roles:
-        print(len(item), *item) # * unpacks the items in the list (might be called spread operator?)
+        output += str(len(item)) + ' '
+        output += ' '.join(map(str, item))  + '\n'
     for item in scenes:
-        print(len(item), *item)
+        output += str(len(item)) + ' '
+        output += ' '.join(map(str, item))  + '\n' 
+
+    print(output)
+    return output
         
-run()
+output = run()
+draw_graph(output)
+
+# python main.py < input.txt
